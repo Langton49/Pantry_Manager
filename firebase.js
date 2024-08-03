3b20bb19c9c2ee9bd0be6ad8e76ee0b6f9a0c83e
Munashe Mukweya <munashemukweya2022@gmail.com>
2024-08-03 03:36:23 -0400

    Created Pantry Manager Repo

---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ34B3Zs3Kx0v6RxfCNMJUMGta2mY62rQ",
+  authDomain: "pantry-manager-4f58a.firebaseapp.com",
+  projectId: "pantry-manager-4f58a",
+  storageBucket: "pantry-manager-4f58a.appspot.com",
+  messagingSenderId: "551189940579",
+  appId: "1:551189940579:web:dc2a46e4d889db1a50eb56",
+  measurementId: "G-73GZ8P6K7E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export {firestore};
